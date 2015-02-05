8709348bed9c291e5cce6094b6cc9f815fbd8ba2
Podkopaev Anton <podkoav239@gmail.com>
2015-02-05 14:54:23 +0300

    Rewrited interval module

--- interval.ml
+++ interval.ml
@@ -1,89 +1,73 @@
-(*
-module type AnalysisLattice =
-sig
-  type t
-  val leq  : t -> t -> bool
-  val join : t -> t -> t
-
-  val incr : t -> t
-  val decr : t -> t
-end
-
-module type Bot =
-sig
-  type t
-  val bot : t
-end
-
-module type LatticeMeet =
-sig
-  type t
-  val meet : t -> t -> t
-end
-
-module type SetOperations =
-sig
-  type t
-  val iszero  : t -> t
-  val notzero : t -> t
-end
- *)
-
-module BorderValue =
+module Interval =
 struct
-  type t = NegInf | Num of int | PosInf
-                                   
+  type t = Bot | NegInf of int | Fin of int * int | PosInf of int | Top
+
+  let pp (i : t) =
+    match i with
+    | Bot -> "O"
+    | Top -> "Z"
+    | NegInf   x -> Printf.sprintf "(-inf, %4i)" x
+    | PosInf   x -> Printf.sprintf "(%4i, +inf)" x
+    | Fin (x, y) -> Printf.sprintf "(%4i, %4i)" x y
+
+  let bot : t = Bot
+
   let leq a b =
     match a, b with
-    | _, PosInf -> true
-    | NegInf, _ -> true
-    | Num  x,  Num y -> x <= y
+    | Bot, _ -> true
+    | _, Top -> true
+    | NegInf     x, NegInf y     -> x <= y
+    | Fin   (_, x), NegInf y     -> x <= y
+    | Fin (xa, ya), Fin (xb, yb) -> xa >= xb && ya <= yb
+    | Fin   (x, _), PosInf y     -> x >= y
+    | PosInf     x, PosInf y     -> x >= y
     | _ -> false
   
   let join a b =
     match a, b with
-    | PosInf, _ -> PosInf
-    | _, PosInf -> PosInf
-    | NegInf, _ -> b
-    | _, NegInf -> a
-    | Num x, Num y -> Num (max x y)
-  
-  let meet a b =
-    match a, b with
-    | PosInf, _ -> b
-    | _, PosInf -> a
-    | NegInf, _ -> NegInf
-    | _, NegInf -> NegInf
-    | Num x, Num y -> Num (min x y)
-  
-  let incr a =
-    match a with
-    | Num x -> Num (x + 1)
-    | _ -> a
-  
-  let decr a =
-    match a with
-    | Num x -> Num (x - 1)
-    | _ -> a
-end
-
-module Interval =
-struct
-  open BorderValue
-  type t = BorderValue.t * BorderValue.t
-
-  let bot : t = (PosInf, NegInf)
-
-  (* Bottom normalization *)
-  let normalize (i : t) : t =
-    match i with
-    | PosInf, PosInf -> bot 
-    | NegInf, NegInf -> bot 
-    | a, b -> if leq a b then i else bot
-
-  let leq a b =
-    let (anl, anr), (bnl, bnr) = normalize a, normalize b in 
+    | Bot, _ -> a
+    | _, Bot -> b
+    | _, Top -> Top
+    | Top, _ -> Top
+    | NegInf x, NegInf y -> NegInf (max x y)
+    | PosInf x, PosInf y -> PosInf (min x y)
+    | PosInf _, NegInf _ -> Top
+    | NegInf _, PosInf _ -> Top
     
-
+    | Fin (xa, ya), Fin (xb, yb) -> Fin (min xa xb, max ya yb)
+    
+    | Fin (_, x), NegInf y -> NegInf (max x y) 
+    | NegInf y, Fin (_, x) -> NegInf (max x y) 
+    
+    | Fin (x, _), PosInf y -> PosInf (min x y) 
+    | PosInf y, Fin (x, _) -> PosInf (min x y) 
+  
+  let iszero i =
+    match i with
+    | Bot -> Bot
+    | Top -> Fin (0, 0)
+    | NegInf x -> if x >= 0 then Fin (0, 0) else Bot
+    | PosInf x -> if x <= 0 then Fin (0, 0) else Bot
+    | Fin (x, y) -> if x <= 0 && y >= 0 then Fin (0, 0) else Bot
+  let notzero i =
+    match i with
+    | Fin (0, 0) -> Bot
+    | Fin (0, x) -> Fin (1, x)
+    | Fin (x, 0) -> Fin (x, -1)
+    | NegInf 0   -> NegInf (-1)
+    | PosInf 0   -> PosInf 1
+    | _ -> i
+  let incr i =
+    match i with
+    | Fin (a, b) -> Fin (a+1, b+1)
+    | NegInf x -> NegInf (x+1)
+    | PosInf x -> PosInf (x+1)
+    | _ -> i
+  let decr i =
+    match i with
+    | Fin (a, b) -> Fin (a-1, b-1)
+    | NegInf x -> NegInf (x-1)
+    | PosInf x -> PosInf (x-1)
+    | _ -> i
 end
 
