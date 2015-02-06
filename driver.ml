840939969ce57c872acfa5785f56982f21276218
Podkopaev Anton <podkoav239@gmail.com>
2015-02-06 17:59:57 +0300

    Added another example

--- driver.ml
+++ driver.ml
@@ -18,20 +18,29 @@ let pr_ex4 =
 
 let pr_ex5 = 
 "inc y
- dec y
- inc y
  dec z
- zero x 3 else 6
+ zero x 1 else 4
  inc z
  dec x
- zero x 6 else 9
+ zero x 4 else 7
  dec z
- zero z 8 else 11
+ zero z 6 else 9
+ stop"
+
+let pr_ex6 = 
+"inc y
+ dec z
+ zero x 4 else 1
+ inc z
+ dec x
+ zero x 7 else 4
+ dec z
+ zero z 9 else 6
  stop"
 
      
 let () =
-  let pr1 = fst (List.hd (progP pr_ex5)) in
+  let pr1 = fst (List.hd (progP pr_ex6)) in
   let module M = AbstractInterpreter(IntervalState) in
   (*let module M = AbstractInterpreter(ParityState) in*)
   M.loop pr1
